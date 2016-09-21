f1672284f8910231b3807bf91912b61cb885db0c
Chenfeng Zhu <zhuchenf@gmail.com>
2016-09-21 03:35:23 +0200

    change the structure of the packages for client part and server part.

--- ServerApp/ecserver/conf/properties_reader.py
+++ ServerApp/ecserver/conf/properties_reader.py
@@ -1,7 +1,14 @@
+# Copyright (C) 2016 Chenfeng Zhu
+#
+# This file is part of the "Energy Consumption" project.
+#
+
 import ConfigParser
+import os
 
 config = ConfigParser.RawConfigParser()
-config.read('config.properties')
+configFile = os.path.join(os.path.abspath(os.path.dirname(__file__)), "config.properties")
+config.read(configFile)
 
 def getSIPPublicAddr():
     public_addr = config.get("SIP", "sip.public_addr")
@@ -10,3 +17,15 @@ def getSIPPublicAddr():
 def getSIPPort():
     port = config.getint("SIP", "sip.port")
     return port
+
+def getCassandraHost():
+    host = config.get("CassandraDB", "cassandra.host")
+    return host
+
+def getCassandraPort():
+    port = config.getint("CassandraDB", "cassandra.port")
+    return port
+
+def getCassandraKeyspace():
+    keyspace = config.get("CassandraDB", "cassandra.keyspace")
+    return keyspace
