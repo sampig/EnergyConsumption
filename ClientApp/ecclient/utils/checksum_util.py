cdc28bc2780eab684de1423545444173d88239d2
Chenfeng Zhu <zhuchenf@gmail.com>
2016-09-27 02:57:01 +0200

    client: checksum_util

--- ClientApp/ecclient/utils/checksum_util.py
+++ ClientApp/ecclient/utils/checksum_util.py
@@ -13,6 +13,17 @@ def getMD5(text):
     return hashlib.md5(text).hexdigest()
 
 # Get the MD5 value of an array
-def getArrayMD5(array):
-    text = "\n".join(array)
+def getArrayMD5(array, delimiter = "\n"):
+    text = delimiter.join(array)
     return getMD5(text)
+
+if __name__ == "__main__":
+    arr = []
+    for i in xrange(0, 44000):
+        v = str(20160909151515) + str('%06d'%i) + ":" + str(i)
+        arr.append(v)
+    print len(arr)
+    print arr[0:10]
+    print getArrayMD5(arr)
+    print getArrayMD5(arr, "|")
+    print getArrayMD5(arr, ",")
