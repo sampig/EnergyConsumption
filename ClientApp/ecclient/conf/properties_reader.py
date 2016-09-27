dbb8e779b53e1f2c8be883d0db4e69a46517822b
Chenfeng Zhu <zhuchenf@gmail.com>
2016-09-27 02:56:31 +0200

    client: properties_reader

--- ClientApp/ecclient/conf/properties_reader.py
+++ ClientApp/ecclient/conf/properties_reader.py
@@ -8,7 +8,7 @@ import ConfigParser
 import os
 
 config = ConfigParser.RawConfigParser()
-configFile = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.properties')
+configFile = os.path.join(os.path.abspath(os.path.dirname(__file__)), "config.properties")
 config.read(configFile)
 
 def getSaveFrequency():
@@ -33,17 +33,23 @@ def getSyncFilenameFmt():
 
 # Get hostname and port of Ostinato
 def getOstinatoConfigHost():
-    hostname = config.get('Ostinato', 'ostinato.hostname')
-    port = config.getint('Ostinato', 'ostinato.port')
+    hostname = config.get("Ostinato", "ostinato.hostname")
+    port = config.getint("Ostinato", "ostinato.port")
     return [hostname, port]
 
 # Get the file of device information
 def getDevicesFileName():
-    filename = config.get('Files', 'file.devices')
+    filename = config.get("Files", "file.devices")
     return filename
 
 # Get the URI of SIP Server
 def getSIPServerURI():
-    serveruri = config.get('SIP', 'sip.serveruri')
+    serveruri = config.get("SIP", "sip.serveruri")
     return serveruri
+
+
+if __name__ == "__main__":
+    print "Save Filename Format: ", getSaveFilenameFmt()
+    print "Ostinato Config: ", getOstinatoConfigHost()
+    print "SIP Server URI: ", getSIPServerURI()
 
